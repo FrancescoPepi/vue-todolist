68cb08303b5be0b99391f734706f6096f756a773
FrancescoPepi <francescopepi16@gmail.com>
2023-09-01 12:07:29 +0200

    fix; add style

--- js/main.js
+++ js/main.js
@@ -17,7 +17,15 @@ createApp({
           done: false,
         },
       ],
-      newTodo: "",
+      newTodo: {
+        item: "",
+        done: false,
+      },
+      error: {
+        msg: "",
+        type: "",
+        done: false,
+      },
     };
   },
   methods: {
@@ -32,11 +40,26 @@ createApp({
       this.todoList.splice(index, 1);
     },
     pushNew() {
-      if (!this.newTodo == "")
-        this.todoList.unshift({
-          item: this.newTodo,
-          done: false,
-        });
+      const newTodoCopy = { ...this.newTodo };
+      if (newTodoCopy.item.length <= 1) {
+        this.error.msg = "l'item è troppo corto";
+        this.error.done = true;
+        console.log("no");
+        return;
+      } else {
+        this.error.done = false;
+      }
+      for (element in this.todoList) {
+        if (this.todoList[element].item.includes(newTodoCopy.item)) {
+          this.error.msg = "l'item inserito è già nella lista";
+          this.error.done = true;
+          console.log("no");
+          return;
+        } else {
+          this.error.done = false;
+        }
+      }
+      this.todoList.unshift(newTodoCopy);
     },
   },
 }).mount("#app");
